1b7098902c5d6338c24993e881dc813b01279eed
k-ghale <kabinghale888@gmail.com>
2026-04-02 15:25:33 -0400

    add: added main and get_book_content function

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 
-print("greetings boots")
+def main():
+    path="./books/frankenstein.txt"
+    book_content = get_books_text(path);
+    print(book_content)
 
+
+def get_books_text(path):
+    with open(path) as f:
+        file_content = f.read()
+    return file_content
+
+
+
+main();
