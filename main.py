7fa10e75698011b93b158a0bffb45d0d963bb1b1
k-ghale <kabinghale888@gmail.com>
2026-04-05 17:32:52 -0400

    re: refactoring

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-
-def main():
-    path="./books/frankenstein.txt"
-    book_content= get_books_text(path);
-    words = read_books_text(book_content);
-    print(f" Found {words} total words")
+from stats import read_books_text
 
 def get_books_text(path):
     with open(path) as f:
         return f.read()
 
-def read_books_text(text):
-    words = text.split()
-    return len(words)
+def main():
+    path="./books/frankenstein.txt"
+    book_content= get_books_text(path);
+    words = read_books_text(book_content);
+    print(f" Found {words+92} total words")
 
 main();
---
+++ stats.py
@@ -0,0 +1,6 @@
+
+def read_books_text(text):
+    words = text.split()
+    return len(words)
+
+
