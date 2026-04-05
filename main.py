ec60332c1559d18e988d5cbce54f2b559f62b2a8
k-ghale <kabinghale888@gmail.com>
2026-04-05 17:52:42 -0400

    add: make a report of it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import read_books_text
 from stats import num_of_each_character
+from stats import chars_dict_to_sorted_list
 
 def get_books_text(path):
     with open(path) as f:
@@ -10,6 +11,21 @@ def main():
     book_content= get_books_text(path);
     words = read_books_text(book_content);
     characters = num_of_each_character(book_content);
-    print(characters);
+    sorted = chars_dict_to_sorted_list(characters)
+    print_report(path, words, sorted)
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main();
